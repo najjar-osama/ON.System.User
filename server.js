bfb122adc7af5048721217f485a75110b2bf0f32
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-17 01:59:33 +0200

    set dev & prod enviornment variables

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "dev" ? "./dev.env" : "./prod.env"
+});
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    process.env.DB_URL,
+    {
+      useNewUrlParser: true,
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD
+    }
+  )
+  .then(() => {
+    console.log(`Connected to DB [env:${process.env.NODE_ENV}] successfully!`);
+  })
+  .catch(error => {
+    console.log(error.message);
+  });
 
 app.get("/", (req, res) => {
   res.send("This is Your Hello World!!");
