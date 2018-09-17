9a89ba086f356662232245b6d01b7131977697d6
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-17 03:18:38 +0200

    set basic routes handlers

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const usersRouteHandler = require("./routes/api/users");
+const profileRouteHandler = require("./routes/api/profile");
+const postsRouteHandler = require("./routes/api/posts");
+
+app.use("/api/users", usersRouteHandler);
+app.use("/api/profile", profileRouteHandler);
+app.use("/api/posts", postsRouteHandler);
+
 mongoose
   .connect(
     process.env.DB_URL,
