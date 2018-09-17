f0ba72b419037781e70aee4015c324ac80017c83
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-18 00:34:31 +0200

    fix typo

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ require("dotenv").config({
 });
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
 
+const app = express();
+// load routes
 const usersRouteHandler = require("./routes/api/users");
 const profileRouteHandler = require("./routes/api/profile");
 const postsRouteHandler = require("./routes/api/posts");
+
+// BodyParser  middelware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
 
 app.use("/api/users", usersRouteHandler);
 app.use("/api/profile", profileRouteHandler);
